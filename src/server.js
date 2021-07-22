b06bb9c52659bf34d8944d193dd6d4696a23781c
Mariana Oliveira <marisouza.cco@gmail.com>
2021-07-22 12:33:49 -0300

    CRUD

--- src/server.js
+++ src/server.js
@@ -1,9 +1,39 @@
 const porta = 8080
 const express = require('express')
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
 const app = express()
 
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get('/produtos', (req,res,next) => {
-    res.send({nome: "notebook", preco: 2894.78 })
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req,res,next) =>{
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req,res,next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+app.put('/produtos/:id', (req,res,next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+app.delete('/produtos/:id', (req,res,next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto)
 })
 
 app.listen(porta, () => {
